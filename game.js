dd25c244a7045463d17f8bc61ac9f90fcb1eeacf
Sahil <sahilwaghmode@gmail.com>
2020-04-26 20:45:50 +0530

    final touch, all worked

--- game.js
+++ game.js
@@ -1,5 +1,4 @@
 
-
 var gamePattern = [];
 var buttonColors = ["red","blue","green", "yellow"];
 var userClickedPattern =[];
@@ -7,25 +6,25 @@ var level=0;
 var started = false;
 var pos = 0;
 
-$(document).keypress(()=>{
-    if(!started){
-        level = 0;
-        nextSequance();
-        $("h1").text("Level " +level);
-        started =    true;   
+$(document).keypress(function() {
+    if (!started) {
+      $("#level-title").text("Level " + level);
+      nextSequance();
+      started = true;
     }
-})
+  });
 
 function nextSequance() {
+    userClickedPattern = [];
     level = level + 1;
-    // console.log("in nextSequance()")
+    $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random()*10);
-    // console.log(randomNumber);
     var index = randomNumber % 4;
     var randomChosenColor = buttonColors[index];
-    // console.log(randomChosenColor);
+
     gamePattern.push(randomChosenColor);
-    console.log(gamePattern);
+    console.log("Game Pattern " + gamePattern);
+
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
     //Audio is class which represent HTML tag audio
     //audio is object iof class Audio which has methods loke play() pause() etc.
@@ -33,54 +32,61 @@ function nextSequance() {
     audio.play();
 }
 
-$(".btn").click(function(){
-    var userChosenColor = this.id;
-    // console.log(userChosenColor);
-    userClickedPattern.push(userChosenColor);
-    console.log(userClickedPattern);
-    var len =userClickedPattern.length;
-    playSound(userChosenColor);
-    animatePress(userChosenColor);
-    checkAnswer(userClickedPattern[len-1]);
-})
+$(".btn").click(function() {
 
-function playSound(name){
-    // console.log("in playSound()");
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+  
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+  
+    checkAnswer(userClickedPattern.length-1);
+});
+
+function playSound(name) {
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
 
-function animatePress(currentColor){
-    // console.log("in animatePress");
-    $("#" + currentColor).addClass("pressed") ;
-    setTimeout(()=>{$("#" + currentColor).removeClass("pressed") ;0}, 100);
-}
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
+  }
+  
 
-function checkAnswer(currentLevel){
-    // console.log(currentLevel);
-    var result
-    if(JSON.stringify(gamePattern)==JSON.stringify(userClickedPattern)){
-        console.log("success"); 
-        result = 1;
-    }
-    else{
-        console.log("you are failure");
-        result = 0;
-    }
-    if(result){
-        setTimeout(() => {
-            nextSequance();
+  function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      console.log("success");
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequance();
         }, 1000);
-    }
-    else{
-        gameOver();
-    }
-}
+      }
+    } else {
+      console.log("wrong");
+      playSound("wrong");
+      $("body").addClass("game-over");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
 
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+      startOver();
+    }
+
+}
 function gameOver(){
     $("body").addClass("game-over");
-    setTimeout(()=> {$("body").removeClass("game-over");},100);
+    setTimeout(()=> {$("body").removeClass("game-over");},200);
     playSound("wrong");
+    $("h1").text("Game Over, Press any key to play again");
+}
+
+function startOver(){
+    level=0;
+    gamePattern = [];
     started = false;
-    $("h1").text("Press any key to play again");
 }
